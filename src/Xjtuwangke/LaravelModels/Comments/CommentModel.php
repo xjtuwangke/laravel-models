93fd3b14705f25d667284937ba2a787a415b563e
wangke <wangke85@gmail.com>
2014-10-13 07:09:11 +0800

    no message

--- src/Xjtuwangke/LaravelModels/Comments/CommentModel.php
+++ src/Xjtuwangke/LaravelModels/Comments/CommentModel.php
@@ -8,6 +8,8 @@
 
 namespace Xjtuwangke\LaravelModels\Comments;
 
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Xjtuwangke\LaravelModels\BasicModel;
 use Xjtuwangke\LaravelModels\Traits\MultiStatusTrait;
 
@@ -19,6 +21,13 @@ class CommentModel extends BasicModel{
 
     protected $table = 'comments';
 
+    public static function _schema( Blueprint $table ){
+        $table = parent::_schema( $table );
+        $table->integer( 'user_id' );
+        $table->morphs( 'commentable' );
+        $table->longText( 'comment' )->nullable();
+    }
+
     public function user(){
         return $this->hasOne( 'Xjtuwangke\LaravelModels\Users\UserModel' , 'id' , 'user_id' );
     }
@@ -27,10 +36,16 @@ class CommentModel extends BasicModel{
         return $this->morphTo();
     }
 
-    public static function createByUser( BasicModel $user , BasicModel $commentable , array $comment ){
+    public static function createByUser( BasicModel $user , $commentable , array $comment ){
         $comment['user_id'] = $user->id;
-        $comment['commentable_type'] = $commentable->getMorphClass();
-        $comment['commentable_id']   = $commentable->id;
+        if( $commentable ){
+            $comment['commentable_type'] = $commentable->getMorphClass();
+            $comment['commentable_id']   = $commentable->id;
+        }
+        else{
+            $comment['commentable_type'] = get_called_class();
+            $comment['commentable_id']   = 0;
+        }
         return static::create( $comment );
     }
 
@@ -41,6 +56,24 @@ class CommentModel extends BasicModel{
             $root = $comentable;
         }
         return $root;
+    }
+
+    public static function lastActiveUsers( $take , $since = '1970-01-01 00:00:00' ){
+        return static::select( DB::raw('count(*) as count, user_id') )
+            ->where( 'status' , '可见' )
+            ->where( 'created_at' , '>=' , $since )
+            ->orderBy( 'created_at' , 'desc' )
+            ->groupBy('user_id')
+            ->take( $take )
+            ->get();
+    }
+
+    public static function countUserComments( BasicModel $user , $since = '1970-01-01 00:00:00' ){
+        return static::select( DB::raw('count(*) as count, user_id') )
+            ->where( 'status' , '可见' )
+            ->where( 'user_id' , $user->getKey() )
+            ->where( 'created_at' , '>=' , $since )
+            ->count();
     }
 
 } 
